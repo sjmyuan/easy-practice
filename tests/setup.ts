ce37fad26543d27ea1af2573b2049b23255d29f0
sjmyuan <shang.jiaming@163.com>
2025-12-20 16:10:51 +0800

    fix the database test stuck

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,28 +1,9 @@
 import '@testing-library/jest-dom';
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
+import 'fake-indexeddb/auto';
 
 // Cleanup after each test case
 afterEach(() => {
   cleanup();
 });
-
-// Mock IndexedDB
-const indexedDB = {
-  open: () => ({
-    onsuccess: null,
-    onerror: null,
-    result: {
-      transaction: () => ({
-        objectStore: () => ({
-          get: () => ({}),
-          put: () => ({}),
-          add: () => ({}),
-          delete: () => ({}),
-        }),
-      }),
-    },
-  }),
-};
-
-global.indexedDB = indexedDB as any;
